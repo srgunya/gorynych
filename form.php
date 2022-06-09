e7bc6073b357f05f9763fdae45e913460014a29f
sergmaximenko <sukavigimalka@mail.ru>
2022-06-09 20:25:28 +0300

    forma

--- form.php
+++ form.php
@@ -1,6 +1,9 @@
-<?php 
+<?php
+
 $to = "jnew1@mail.ru";
 $from = trim($_POST['email']);
+
+$subject = 'Бронирование столика';
 
 $name = htmlspecialchars($_POST['name']);
 $person = htmlspecialchars($_POST['person']);
@@ -16,17 +19,14 @@ $tel = urldecode($tel);
 $date = urldecode($date);
 $comment = urldecode($comment);
 
-$name = trim($name);
-$person = trim($person);
-$email = trim($email);
-$tel = trim($tel);
-$date = trim($date);
+$name =trim($name);
+$person =trim($person);
+$email =trim($email);
+$tel =trim($tel);
+$date =trim($date);
 $comment = trim($comment);
 
-$subject = "Бронирование столика";
-$message = "$name" . "\r\n" . "$person" . "\r\n" . "$email" . "\r\n" . "$tel" . "\r\n" . "$date" . "\r\n" . "$comment";
-
-$headers = "From: $from" . "\r\n" . "Reply-To: $from" . "\r\n" . "X-Mailer: PHP/" . phpversion();
-
+$headers = "From: $form" . "\r\n" . "Reply-To: $from" . "\r\n" . "X-Mailer: PHP/" . phpversion();
+$message = "Имя: $name" . "\r\n" . "Колличество персон: $person" . "\r\n" . "Email: $email" . "\r\n" . "Телефон: $tel" . "\r\n" . "Дата: $date" . "\r\n" . "Комментарий: $comment";
 mail($to, $subject, $message, $headers);
 ?>
